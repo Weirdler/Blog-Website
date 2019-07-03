8bee3dd4a38ab4ec9ef1a487f5e43913a962c9a8
Bren <captainbmq@gmail.com>
2019-07-03 12:47:17 -0400

    afjksdsfsf

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+
+app.set("view engine","ejs");
+app.use(express.static('./public'));
+
+app.get('/home', function(req, res){
+    console.log('bitch')
+    res.render('home');
+});
+app.get('/home.ejs', function(req, res){
+    console.log('bitch')
+    res.render('home');
+});
+
+app.get('/bbloq.ejs', function(req, res){
+    console.log('megafaggot'),
+    res.render('bbloq')
+});
+app.get('/pbloq.ejs', function(req, res){
+    console.log('bith lasag'),
+    res.render('bbloq')
+});
+
+app.listen(3000, function(err){
+    if (err){
+        console.log(err)
+    }
+    console.log('u mega gay times 3000')
+});
